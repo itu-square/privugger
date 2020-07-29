19c9b1307d3aa13425ac72c7c7b830c91266b77f
Pluttodk <mathiasoliverjorgensen@hotmail.com>
2020-07-29 14:15:57 +0200

    Updated prior-generator to work with a list of the distributions, and to produce a graph of the probability of Alice being in the dataset

--- average.py
+++ average.py
@@ -2,10 +2,13 @@ import numpy as np
 from matplotlib import pyplot as plt
 import pymc3 as pm
 import seaborn as sns
-from hypothesis import given, settings, Phase, strategies as st
+from hypothesis import given, settings, Phase, HealthCheck, strategies as st
 from prior_generator import ProbabilityGenerators as pg
+from prior_generator import ProbabilityDistributions as pd
 from functools import reduce
-
+import logging
+logger = logging.getLogger("pymc3")
+logger.setLevel(logging.ERROR)
 ########################
 ## Program to analyze ##
 ########################
@@ -15,7 +18,10 @@ def alpha(database):
             /
             len(database))
 
-@settings(max_examples=20, deadline=None, phases=[Phase.generate])
+hist = []
+labels = []
+
+@settings(max_examples=2, deadline=None, phases=[Phase.generate],suppress_health_check=[HealthCheck.too_slow])
 @given(st.data())
 def test_with_given(data):
     """
@@ -27,23 +33,27 @@ def test_with_given(data):
         ## Prior ##
         ###########
         # Database
-        N = 20 # number of entries in the database
+        N = 10 # number of entries in the database
         x = np.empty(N+1, dtype=object)  
 
         # Alice entry
-        age_alice_database = data.draw(pg.UniformDist(name="alice_age"))
+        age_alice_database = pm.Uniform("alice_age", lower=0, upper=100)
+        name_age_alice = "alice_age"
+        # age_alice_database = data.draw(pg.FloatGenerator(name="alice_age"))
+        # name_age_alice = age_alice_database[2]
         name_alice_database = pm.Constant("name_alice_database", 0)   
 
         # Add Alice in database
         x[0] = (name_alice_database, age_alice_database)
 
         # Other users
-        age = data.draw(pg.NormalDist(name="Age", shape=N))
-        name = pm.DiscreteUniform("name", 0, 5, shape=N)
+        age = [data.draw(i) for i in data.draw(pg.ListDist(name="Age",length=N))]
+        # age = data.draw(pg.NormalDist(name="Age", shape=N))
+        name = [data.draw(i) for i in data.draw(pg.IntListDist(name="Name", length=N))]
 
         # Add users to the database
         for i in range(0, N):
-            x[i+1] = (name[i], age[i])
+            x[i+1] = (name[i][0], age[i][0])
 
             
         #########################
@@ -52,26 +62,33 @@ def test_with_given(data):
         average = pm.Deterministic("average", alpha(x))
 
         
-        #################
-        ## Observation ##
-        #################
-        obs = pm.Normal('obs', mu=average, sigma=.05, observed=55.3)
+        # #################
+        # ## Observation ##
+        # #################
+        # obs = pm.Normal('obs', mu=average, sigma=.05, observed=55.3)
 
         
         ##############
         ## Sampling ##
         ##############
-        num_samples = 5000
-        prior = pm.sample_prior_predictive(num_samples)
+        num_samples = 10
+        # # prior = pm.sample_prior_predictive(num_samples)
         trace = pm.sample(num_samples, cores=1)
 
         
-        #############
-        ## Queries ##
-        #############
-        print("P(22.4 < average(x) < 22.5): " + str(np.mean((22.4 < trace["average"])*(trace["average"] < 22.5))))
-        print("P(alice < 18): " + str(np.mean(trace["alice_age"] < 18)))
-
+        # #############
+        # ## Queries ##
+        # #############
+        # print("P(22.4 < average(x) < 22.5): " + str(np.mean((22.4 < trace["average"])*(trace["average"] < 22.5))))
+        print("P(alice < 18): " + str(np.mean(trace[name_age_alice] < 18)))
+        string_to_add = ""
+        for dist in age:
+            for i, parameters in enumerate(dist[1]):
+                string_to_add += f"{parameters} "
+        normal = string_to_add.count("normal")
+        uni = string_to_add.count("Uniform")
+        labels.append(f"Age : (#Normal {normal}, #Uniform {uni})")
+        hist.append(np.mean(trace[name_age_alice] < 18))
         
         # ##############
         # ## Plotting ##
@@ -82,3 +99,15 @@ def test_with_given(data):
         # plt.show()
 
 test_with_given()
+print(hist)
+print(labels)
+
+fig,ax = plt.subplots()
+ax.barh(np.arange(len(hist)), hist,align="center")
+ax.set_yticks(np.arange(len(hist)))
+ax.set_yticklabels(labels)
+ax.set_xlabel("p(alice < 18)")
+ax.set_ylabel("Number of various distributions used to mimic the Database")
+ax.set_title("Performance of difference distributions as the database")
+
+plt.show()
--- prior_generator.py
+++ prior_generator.py
@@ -6,89 +6,116 @@ import arviz as az
 from matplotlib import pyplot as plt
 import random
 
-def UniformHelper(name="Uniform", size=(0,10)):
+class ProbabilityDistributions:
     """
-    A helper method for uniform
+    A "enum" class to mimic all the possible distributions
     """
-    return pm.distributions.Uniform(name, lower=size[0], upper=size[1])
+    NormalDist, UniformDist, BinomialDist, BernoulliDist, GeometricDist = range(5)
 
 class ProbabilityGenerators:
     @staticmethod
-    def IntDist(name="Int Distribution"):
-        """
-        A method for generating a distribution to mimic int distribution
+    def IntListDist(name="Int Distribution", length=1, possible_dist = [i for i in range(2,5)]):
+        f = (lambda r: ProbabilityGenerators.IntDist(r, name, possible_dist))
+        return st.lists(st.builds(f, r=st.randoms(use_true_random=True)), min_size=length, max_size=length)
 
-        Distribution Generators
+    @staticmethod
+    def ListDist(name="", length=1, possible_dist = []):
+        f = (lambda r: ProbabilityGenerators.FloatGenerator(name, r, possible_dist))
+        return st.lists(st.builds(f, r=st.randoms(use_true_random=True)), min_size=length, max_size=length)
+
+    @staticmethod
+    def IntDist(rand, name="Int Distribution", distri = []):
+        """
+        A method for generating a distributions to mimic int distribution
+
+        distributions Generators
         ----------
         n : int 
             The number of cases for a binomial distribution
         p : float (between 0.0 and 1.0)
             The probability of success for the given test
         """
-        dist = random.choice([0,1,2])
-        if dist == 0:
-            return builds(pm.distributions.Binomial, s.from_regex(f"^{name}$"), integers(min_value = 1), floats(min_value = 0.001, max_value=1))
-        elif dist == 1:
-            return builds(pm.distributions.Bernoulli, st.from_regex(f"^{name}$"), floats(min_value = 0.001, max_value=1))
-        else:
-            return builds(pm.distributions.Geometric, st.from_regex(f"^{name}$"), floats(min_value = 0.001, max_value=1))
-    @staticmethod
-    def FloatDist(name=""):
-        """
-        A distribution for randomly choosing a float distribution
-
-        TODO: 
-            - Has to include all distributions that can mimic floats
-            - Include additional parameters
-        """
-        numberOfDist = 10
-        dist = random.choice([0,1])
-        if not dist:
-            return ProbabilityGenerators.NormalDist(name)
-        else:
-            return ProbabilityGenerators.UniformDist(name)
-    @staticmethod
-    def NormalDist(name="normal", shape=1):
-        """
-        A method for generating a distributions to mimics floats of any size
-
-        Parameters
-        ----------
-        name : string
-            The name of the distribution
-        shape : int
-            The shape of the input values
-        """
-        return builds(pm.distributions.Normal, 
-                        st.from_regex(f"^{name}$"), 
-                        mu=floats(allow_infinity=False, 
-                            allow_nan=False), 
-                        sigma=floats(min_value=0.001,  
-                            allow_infinity=False, 
-                            allow_nan=False), shape=integers(min_value=shape, max_value=shape))
+        dist = rand.choice(distri)
+        if dist == ProbabilityDistributions.BinomialDist:
+            return builds(ProbabilityGenerators.BinomialDist, 
+                name=st.from_regex(f"^{name}$"), 
+                n=integers(min_value = 1), 
+                p=floats(min_value = 0.001, max_value=1),
+                uuids=st.uuids())
+        elif dist == ProbabilityDistributions.BernoulliDist:
+            return builds(ProbabilityGenerators.BernoulliDist, 
+                name=st.from_regex(f"^{name}$"), 
+                p=floats(min_value = 0.001, max_value=1),
+                uuids=st.uuids())
+        elif dist == ProbabilityDistributions.GeometricDist:
+            return builds(ProbabilityGenerators.GeometricDist, 
+                name=st.from_regex(f"^{name}$"), 
+                p=floats(min_value = 0.001, max_value=1),
+                uuids=st.uuids())
     
     @staticmethod
-    def UniformDist(name="Uniform"):
-        """
-        A method for generating Uniform distribution
-        - Generates distribution for floats
+    def BinomialDist(name, n, p, uuids):
+        return (pm.distributions.Binomial(name=name+str(uuids), n=n, p=p), ["Binomial", n,p], name+str(uuids))
+    
+    @staticmethod
+    def BernoulliDist(name, p, uuids):
+        return (pm.distributions.Bernoulli(name=name+str(uuids), p=p), ["Bernoulli", p], name+str(uuids))
+        
+    @staticmethod
+    def GeometricDist(name, p, uuids):
+        return (pm.distributions.Geometric(name=name+str(uuids), p=p), ["Geometric", p], name+str(uuids))
 
-        Parameters
-        ----------
-        name : string
-            Name of the distribution 
-        """
-        return builds(UniformHelper, 
+    @staticmethod
+    def FloatGenerator(name, rand, possible_dist = [i for i in range(2)]):
+        if not len(possible_dist):
+            possible_dist = [i for i in range(2)]
+        dist = rand.choice(possible_dist)
+        if dist == ProbabilityDistributions.NormalDist:
+            return builds(ProbabilityGenerators.NormalDist, name=st.from_regex(f"^{name}$"),
+                mu = st.floats(allow_infinity=False,
+                    allow_nan=False,
+                    min_value=-10000,
+                    max_value=10000),
+                sigma=st.floats(min_value=0.001,
+                    allow_infinity=False, 
+                    allow_nan=False,
+                    max_value=10000),
+                uuids=st.uuids()
+            )
+        elif dist == ProbabilityDistributions.UniformDist:
+            return builds(ProbabilityGenerators.UniformDist,
                 name=st.from_regex(f"^{name}$"),
-                size=(st.tuples(integers(), integers())
+                uuids = st.uuids(),
+                size=(st.tuples(st.integers(min_value=-10000, max_value=10000), st.integers(min_value=-10000, max_value=10000))
                     .map(sorted)
-                    .filter(lambda x: x[0] < x[1])) 
-                )
+                    .filter(lambda x: x[0] < x[1]))
+            )
+
+    @staticmethod
+    def NormalDist(name, uuids, mu, sigma):
+        return (pm.distributions.Normal(name=name+str(uuids), mu=mu, sigma=sigma), ["normal", mu,sigma], name+str(uuids))
+
+    @staticmethod
+    def UniformDist(name, uuids, size):
+        return (pm.distributions.Uniform(name+str(uuids), lower=size[0], upper=size[1]), ["Uniform", size[0], size[1]], name+str(uuids))
+
+
+    @staticmethod
+    def FloatDist(name, rand, mu, sigma, uuids, size, distri = []):
+        """
+        A distributions for randomly choosing a float distribution
+            - Has to include all distributions that can mimic floats
+        """
+        dist = random.choice(distri)
+        if dist == ProbabilityDistributions.NormalDist:
+            return (pm.distributions.Normal(name=name+str(uuids), mu=mu, sigma=sigma), ["normal", mu,sigma], name+str(uuids))
+        elif dist == ProbabilityDistributions.UniformDist:
+            return (pm.distributions.Uniform(name+str(uuids), lower=size[0], upper=size[1]), ["Uniform", size[0], size[1]], name+str(uuids))
 
     @staticmethod
     def SingleDigitFloatDist(name="SingleDigitFloats"):
         """
-        A distribution for values that spreads between 0 and 1
+        A distributions for values that spreads between 0 and 1
 
         Parameters
         ----------
@@ -102,32 +129,11 @@ class ProbabilityGenerators:
     @staticmethod
     def PositiveFloatDist(name="Non-negative Floats"):
         """
-        A distribution to mimic all values that are larger than 0
+        A distributions to mimic all values that are larger than 0
 
         Parameters
         ----------
         Lambda : float
-            The incremental rate of the distribution (has to be larger than 0)
+            The incremental rate of the distributions (has to be larger than 0)
         """
         return builds(pm.distributions.Exponential, st.from_regex(f"^{name}$"), floats(min_value=0.001))
-
-    @staticmethod
-    def GeneralDist(values):
-        """
-        Ideally the function that will return a generator for the distribution needed
-
-        TODO:
-            - Support numpy values, for more in depth features
-        """
-        if isinstance(values, int):
-            return self.IntDist()
-        elif isinstance(values, float):
-            # TODO: Figure out how values actually is supposed to be feeded to the method
-            if 1 > values > 0:
-                return self.SingleDigitFloatDist()
-            elif values > 0:
-                return self.PositiveFloatDist()
-            else:
-                return self.FloatDist() 
-        else:
-            return None
