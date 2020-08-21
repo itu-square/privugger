314a7a9e065617f57651f9363caba576bad84e1f
Pluttodk <mathiasoliverjorgensen@hotmail.com>
2020-08-21 13:56:39 +0200

    Made the average now write to a csv file (will be used to test for more extisive test. Additionally added back the original average

--- average.py
+++ average.py
@@ -21,7 +21,7 @@ def alpha(database):
 hist = []
 labels = []
 
-@settings(max_examples=4, deadline=None, phases=[Phase.generate],suppress_health_check=[HealthCheck.too_slow])
+@settings(max_examples=10, deadline=None, phases=[Phase.generate],suppress_health_check=[HealthCheck.too_slow])
 @given(st.data())
 def test_with_given(data):
     """
@@ -33,23 +33,21 @@ def test_with_given(data):
         ## Prior ##
         ###########
         # Database
-        N = 1 # number of entries in the database
+        N = 10 # number of entries in the database
         x = np.empty(N+1, dtype=object)  
 
         # Alice entry
         age_alice_database = pm.Uniform("alice_age", lower=0, upper=100)
         name_age_alice = "alice_age"
-        # age_alice_database = data.draw(pg.FloatGenerator(name="alice_age"))
-        # name_age_alice = age_alice_database[2]
         name_alice_database = pm.Constant("name_alice_database", 0)   
 
         # Add Alice in database
         x[0] = (name_alice_database, age_alice_database)
 
         # Other users
-        age = [data.draw(i) for i in data.draw(pg.FloatList(name="Age",length=N, possible_dist=[pd.Normal,pd.Triangular]))]
-        # age = data.draw(pg.NormalDist(name="Age", shape=N))
-        name = [data.draw(i) for i in data.draw(pg.IntList(name="Name", length=N, possible_dist=[pd.Binomial]))]
+        # age = pm.distributions.Normal(name="Age", mu=0, sigma=0.001,shape=N)
+        age = [data.draw(i) for i in data.draw(pg.FloatList(name="Age",length=N))]
+        name = [data.draw(i) for i in data.draw(pg.IntList(name="Name", length=N))]
 
         # Add users to the database
         for i in range(0, N):
@@ -79,37 +77,37 @@ def test_with_given(data):
         # #############
         # ## Queries ##
         # #############
-        # print("P(22.4 < average(x) < 22.5): " + str(np.mean((22.4 < trace["average"])*(trace["average"] < 22.5))))
+        output = open("output.csv", "a")
+        print("P(22.4 < average(x) < 22.5): " + str(np.mean((22.4 < trace["average"])*(trace["average"] < 22.5))))
         print("P(alice < 18): " + str(np.mean(trace[name_age_alice] < 18)))
-        string_to_add = []
-        for dist in age:
-            for i, parameters in enumerate(dist[1]):
-                string_to_add.append(f"{parameters} ")
-        ind = set(string_to_add)
-        name = ""
-        for n in ind:
-            name += f"{n} : {string_to_add.count(n)}"
-        labels.append(n)
-        hist.append(np.mean(trace[name_age_alice] < 18))
-        
-        # ##############
-        # ## Plotting ##
-        # ##############
-        # sns.distplot(prior["alice_age"], label="Prior", hist=False)
-        # ax = sns.distplot(trace["alice_age"], label="Posterior", hist=False)
-        # ax.legend()
-        # plt.show()
+        output.write(f"{N}!")
+        output.write(str(np.mean((22.4 < trace["average"])*(trace["average"] < 22.5))) + "!")
+        output.write(str(np.mean(trace[name_age_alice] < 18)) + "!")
+        par1 = "["
+        par2 = "["
+        for dist1,dist2 in zip(age,name):
+            par1 = par1 + "{"
+            par2 = par2 + "{"
+            for i, (parameters1, parameters2) in enumerate(zip(dist1[1],dist2[1])):
+                par1 = par1 + f"{parameters1},"
+                par2 = par2 + f"{parameters2},"
+            par1 = par1 + "},"
+            par2 = par2 + "},"
+        par1 = par1 + "]"
+        par2 = par2 + "]"
+        output.write(f"{par1}!{par2}")
+        output.write("\n")
 
 test_with_given()
-print(hist)
-print(labels)
+# print(hist)
+# print(labels)
 
-fig,ax = plt.subplots()
-ax.barh(np.arange(len(hist)), hist,align="center")
-ax.set_yticks(np.arange(len(hist)))
-ax.set_yticklabels(labels)
-ax.set_xlabel("p(alice < 18)")
-ax.set_ylabel("Number of various distributions used to mimic the Database")
-ax.set_title("Performance of difference distributions as the database")
+# fig,ax = plt.subplots()
+# ax.barh(np.arange(len(hist)), hist,align="center")
+# ax.set_yticks(np.arange(len(hist)))
+# ax.set_yticklabels(labels)
+# ax.set_xlabel("p(alice < 18)")
+# ax.set_ylabel("Number of various distributions used to mimic the Database")
+# ax.set_title("Performance of difference distributions as the database")
 
-plt.show()
+# plt.show()
