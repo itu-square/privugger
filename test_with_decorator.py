3cb1859fbc9cd3c2bd3c93ca2479a5d6f2b56a89
Pluttodk <mathiasoliverjorgensen@hotmail.com>
2020-09-09 10:55:13 +0200

    Update to new setup

--- test_with_decorator.py
+++ test_with_decorator.py
@@ -1,21 +1,18 @@
 import pymc3 as pm
 from hypothesis import given, settings, HealthCheck, Phase, strategies as st
-from prior_generator import ProbabilityGenerators as pg
-from prior_generator import ProbabilityDistributions as pd
+from privugger import Analyze
 import matplotlib.pyplot as plt
 import random
 from functools import reduce
 
 import inspect
 from typing import List, Tuple
-@pg.Analyze([(1,2.0)])
-
+@Analyze([(1,2.0)],N=20, max_examples=1, num_samples=1000)
 def alpha(database):
     return (reduce((lambda i, j: i + j),
                    list(map(lambda i: i[1], database)))
             /
             len(database))
-
 
 trac = alpha()
 print(trac)
