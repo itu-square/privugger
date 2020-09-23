8e8cafaf9e518610f9c5dadd934f7e25df421372
raulpardo <raup@itu.dk>
2020-09-23 21:25:01 +0200

    Differentially private version of averge

---
+++ average-without-hypothesis-DP.py
@@ -0,0 +1,79 @@
+import numpy as np
+from matplotlib import pyplot as plt
+import pymc3 as pm
+import seaborn as sns
+from functools import reduce
+
+########################
+## Program to analyze ##
+########################
+def alpha_dp(database,param_ε):
+    Δalpha=100/len(database)
+    ε=param_ε
+    b = Δalpha/ε
+    laplace_noise = pm.Laplace("laplace_noise",mu=0,b=b)
+    return (reduce((lambda i, j: i + j),
+                   list(map(lambda i: i[1], database)))
+            /
+            len(database)) + laplace_noise
+
+
+with pm.Model() as model:
+
+    ###########
+    ## Prior ##
+    ###########
+    # Database
+    N = 20 # number of entries in the database
+    x = np.empty(N+1, dtype=object)  
+
+    # Alice entry
+    age_alice_database = pm.Uniform("alice_age", lower=0, upper=100)
+    name_alice_database = pm.Constant("name_alice_database", 0)   
+
+    # Add Alice in database
+    x[0] = (name_alice_database, age_alice_database)
+
+    # Other users
+    age = pm.Normal("age", mu=55.2, sigma=3.5, shape=N)
+    name = pm.DiscreteUniform("name", 0, 5, shape=N)
+
+    # Add users to the database
+    for i in range(0, N):
+        x[i+1] = (name[i], age[i])
+
+        
+    #########################
+    ## Output distribution ##
+    #########################    
+    average = pm.Deterministic("average", alpha_dp(x,.2))
+
+    
+    #################
+    ## Observation ##
+    #################
+    obs = pm.Normal('obs', mu=average, sigma=.05, observed=55.3)
+
+    
+    ##############
+    ## Sampling ##
+    ##############
+    num_samples = 5000
+    prior = pm.sample_prior_predictive(num_samples)
+    trace = pm.sample(num_samples, cores=4)
+
+    
+    #############
+    ## Queries ##
+    #############
+    print("P(22.4 < average(x) < 22.5): " + str(np.mean((22.4 < trace["average"])*(trace["average"] < 22.5))))
+    print("P(alice < 18): " + str(np.mean(trace["alice_age"] < 18)))
+
+    
+    ##############
+    ## Plotting ##
+    ##############
+    sns.distplot(prior["alice_age"], label="Prior", hist=False)
+    ax = sns.distplot(trace["alice_age"], label="Posterior", hist=False)
+    ax.legend()
+    plt.savefig("result-alice-age.pdf")
