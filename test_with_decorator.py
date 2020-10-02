a9e9ef7f8a5a16b996be5a4924c3feae72f58535
Pluttodk <mathiasoliverjorgensen@hotmail.com>
2020-10-02 14:06:44 +0200

    Updated decorator to work more generally

--- test_with_decorator.py
+++ test_with_decorator.py
@@ -8,12 +8,22 @@ from typing import *
 import inspect
 from typing import List, Tuple
 
-@Analyze(List[Tuple[int, float]], N=20, max_examples=1, num_samples=1000)
-def alpha(database):
+@Analyze(N=20, max_examples=1, num_samples=1000)
+def alpha(database : List[Tuple[int, float]]) -> List[Tuple[int, float]]:
     return (reduce((lambda i, j: i + j),
                    list(map(lambda i: i[1], database)))
             /
             len(database))
+# @Analyze(N=20, max_examples=1, num_samples=1000)
+# def alpha_dp(database: List[Tuple[int, float]],param_ε: float) -> List[Tuple[int, float]]:
+#     Δalpha=100/len(database)
+#     ε=param_ε
+#     b = Δalpha/ε
+#     laplace_noise = pm.Laplace("laplace_noise",mu=0,b=b)
+#     return (reduce((lambda i, j: i + j),
+#                    list(map(lambda i: i[1], database)))
+#             /
+#             len(database)) + laplace_noise
 
 trac = alpha()
 print(trac)
