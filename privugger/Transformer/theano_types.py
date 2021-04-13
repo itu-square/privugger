41c45cf92a099ae3ba9469a450a4d65ee2157292
RasmusCarl <rroe@itu.dk>
2021-04-13 11:53:21 +0200

    removed old folder

--- privugger/Transformer/theano_types.py
+++
@@ -1,9 +0,0 @@
-
-
-class TheanoToken():
-    int_vector = "VectorI"
-    float_vector = "VectorF"
-    int_scalar = "int"
-    float_scalar = "float"
-    int_matrix = "MatrixI"
-    float_matrix = "MatrixD"
