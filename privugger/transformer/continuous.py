c79e8484b257594f2ed7bb66ac62f276c8ebad75
RasmusCarl <rroe@itu.dk>
2021-09-22 11:02:37 +0200

    hierarchial model for continuous distributions

--- privugger/transformer/continuous.py
+++ privugger/transformer/continuous.py
@@ -65,7 +65,7 @@ class Uniform(Continuous):
             return pm.Uniform(name, lower=lower, upper=upper, shape=self.num_elements)
 	
     def get_params(self):
-    	return (self.lower, self.upper)
+    	return [self.lower, self.upper]
     
     def scipy_dist(self, name):
         dist = (lambda siz : st.uniform(self.lower, self.upper-self.lower).rvs(siz)) if self.num_elements == -1 else (lambda siz: st.uniform(self.lower, self.upper-self.lower).rvs((self.num_elements, siz)))
@@ -107,7 +107,7 @@ class Normal(Continuous):
         else:
             return pm.Normal(name, mu=mu, sigma=std, shape=self.num_elements)
     def get_params(self):
-    	return (self.mu, self.std)
+    	return [self.mu, self.std]
 
     def scipy_dist(self, name):
         dist = (lambda siz : st.norm(self.mu, self.std).rvs(siz)) if self.num_elements == -1 else (lambda siz: st.norm(self.mu, self.std).rvs((self.num_elements, siz)))
@@ -123,13 +123,18 @@ class Exponential(Continuous):
         self.is_hyper_param = is_hyper_param
 
     def pymc3_dist(self, name, hypers):
+        lam = self.lam
+        if(len(hypers) == 1):
+            hyper_dist = hypers[0][0]
+            hyper_name = hypers[0][1]
+            lam = hyper_dist.pymc3_dist(hyper_name, [])
         if(self.num_elements==-1):
-            return pm.Exponential(name, lam=self.lam)
+            return pm.Exponential(name, lam=lam)
         else:
-            return pm.Exponential(name, lam=self.lam, shape=self.num_elements)
+            return pm.Exponential(name, lam=lam, shape=self.num_elements)
 
     def get_params(self):
-    	return (self.lam)
+    	return [self.lam]
     	
     def scipy_dist(self, name):
         dist = (lambda siz : st.expon(self.lam).rvs(siz)) if self.num_elements == -1 else (lambda siz: st.expon(self.lam).rvs((self.num_elements, siz)))
@@ -139,20 +144,39 @@ class Exponential(Continuous):
 class Beta(Continuous):
     
     def __init__(self, name, alpha=1, beta=1, num_elements=-1, is_hyper_param=False):
-        self.alpha=alpha
-        self.name = name
-        self.beta=beta
+        self.alpha = alpha
+        self.name  = name
+        self.beta  = beta
         self.num_elements=num_elements
         self.is_hyper_param = is_hyper_param
 
     def pymc3_dist(self, name, hypers):
+        alpha = self.alpha
+        beta  = self.beta
+        if(len(hypers) == 1):
+                hyper_dist = hypers[0][0]
+                hyper_name = hypers[0][1]
+                idx = hypers[0][2]
+                if(idx == 0):
+                    alpha = hyper_dist.pymc3_dist(hyper_name, [])
+                else:
+                    beta = hyper_dist.pymc3_dist(hyper_name, [])
+        elif(len(hypers) == 2):
+                hyper_dist_1 = hypers[0][0]
+                hyper_name_1 = hypers[0][1]
+                hyper_dist_2 = hypers[1][0]
+                hyper_name_2 = hypers[1][1]
+                alpha = hyper_dist_1.pymc3_dist(hyper_name_1, [])
+                beta = hyper_dist_2.pymc3_dist(hyper_name_2, [])
+
+
         if(self.num_elements==-1):
             return pm.Beta(name, alpha=self.alpha, beta=self.beta)
         else:
             return pm.Beta(name, alpha=self.alpha, beta=self.beta, shape=self.num_elements)
 	
     def get_params(self):
-    	return (self.alpha, self.beta)
+    	return [self.alpha, self.beta]
     	
     def scipy_dist(self, name):
         dist = (lambda siz : st.beta(self.alpha,self.beta).rvs(siz)) if self.num_elements == -1 else (lambda siz: st.beta(self.alpha,self.beta).rvs((self.num_elements, siz)))
