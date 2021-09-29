50fcd8974a9f02f002da426dd31ab67f0fd78874
raulpardo <raup@itu.dk>
2021-09-29 15:49:38 +0200

    updated mutual information function to work on inferencedata and take a parameter var_names

--- privugger/measures/mutual_information.py
+++ privugger/measures/mutual_information.py
@@ -5,23 +5,25 @@ from sklearn.feature_selection import mutual_info_regression
 
 
 def mi_sklearn(
-        t_secret,
-        t_output,
+        trace,
+        var_names,
         disc_features=False,
         log2=True,
         n_neigh=20
 ):
-    """Binding for estimating mutual information using the `mutual_info_regression` function from `sklearn`.
+    """
+    Binding for estimating mutual information using the `mutual_info_regression` function from `sklearn`.
+
+    Concretely, this function computes mutual information as I(var_names[0]; var_names[1]).      
    
 
     Parameters
     ----------
-    t_secret: array of samples
-        array of samples of the secret variable
-    t_output: array of samples
-        array of samples of the output variable
-    data: az.InferenceData object
-        InferenceData object containing the posterior/prior data.
+    trace : az.InferenceData object
+        InferenceData object containing the samples for variables of interest
+    var_names: String \times String
+        String array with two elements indicating the variables used to compute mutual information.
+        Note that the array must contain exactly two elements.
     disc_features : bool
         Boolean indicating whether the domain of the secret is discerete. Default False.
     log2: bool
@@ -34,8 +36,9 @@ def mi_sklearn(
     result : float
         Mutual information in log2 or ln.
     """
-    mi_nat = mutual_info_regression(t_secret.reshape(-1,1),
-                                    t_output,
+    assert len(var_names)==2, "var_names must contain exactly two elements"
+    mi_nat = mutual_info_regression(trace.posterior[var_names[0]].values.flatten().reshape(-1,1),
+                                    trace.posterior[var_names[1]].values.flatten(),
                                     discrete_features=disc_features,
                                     n_neighbors=n_neigh)
     result = mi_nat/np.log(2) if log2 else mi_nat
