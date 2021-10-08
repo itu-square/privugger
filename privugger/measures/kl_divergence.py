32d69510c17e35d19c0acd2e21ab8c0d9b480e1c
pjdaly <p.j.daly1@outlook.com>
2021-10-08 13:03:52 +0200

    discrete divergences added

--- privugger/measures/kl_divergence.py
+++ privugger/measures/kl_divergence.py
@@ -1,35 +1,64 @@
 import numpy as np
+import portion as p
 from scipy.special import kl_div, entr, rel_entr
 
-__all__ = [
-    "Entropies",
-    "InferenceData"
-]
-class InferenceData:
-    def __init__(self, trace, priors=[]):
-        self.trace = trace
-        self.priors = priors
-    
-    def X(self):
-        p = self.priors
-        x = [self.trace['posterior'][f'{p}'].values for str in p]
-        return x
-    
-    def Y(self):
-        y = self.trace['posterior']['output'].values
-        return  
 
-class Entropies(InferenceData):
-    def __init__(self, trace, priors):
-        super().__init__(trace, priors)
-        self.x = super().X()
-        self.y = super().Y()
+class Divergence:
+    """ 
+    Calculate the divergences between two probablility measures P and Q. 
+    Using scipy.special functions 'entr', 'rel_entr' and 'kl_div' for discrete data.
+    Using the estimation of the Radon-Nikodym derivative via a data-dependent partition as described in;
+    https://doi-org.ep.ituproxy.kb.dk/10.1109/TIT.2005.853314
+    """
+    
+    def __init__(self, data_class=None):
+        self.data_class = data_class
+    """
+    Parameter
+    ------------
+    Data Class : String
+        Data classification, 'discrete' or 'continuous'. Default Value is None.
+    """
+    
+    if self.data_class == 'discrete':
+        """
+        Parameters
+        -----------
+
+        P, Q : Array_like
+        Probability measures 
+
+        Discrete 
+        ---------------------
+
+        entr(P) = -P*log(P) for P > 0, 0 for P = 0 and -∞ otherwise
         
-    def entropy(self):
-        return entr(self.x)
+        rel_entr(P, Q) = P*log(P/Q) for P>0 and Q>0, 0 for P = 0 and Q ≥ 0 and ∞  otherwise
+        
+        Kl_div(P, Q) = P*log(P/Q) - P + Q for P>0 and Q>0, 0 for P = 0 and Q ≥ 0 and ∞   otherwise
+        
+        """
+        @property
+        def entropy(self, P):
+            return entr(P)    
+        
+        @property
+        def rel_entropy(self, P, Q):
+            return rel_entr(P, Q)
+        
+        @property
+        def kl_divergence(self, P, Q):
+            return  kl_div(P, Q) 
+
+    elif self.data_class == 'continous' :
+
+        def kl_divergence(self, P, Q, l_m):
             
-    def relative_entropy(self):
-        return rel_entr(self.x, self.y)
+            n = np.size(P)
+            m = np.size(Q)
             
-    def kl_divergence(self):
-        return kl_div(self.x, self.y)
+            T_n = n/l_m
+            T_m = m/l_m
+            
+            P_I = [np.arange(-np.inf, P[lm])]
+            Q_I = [np.arange(-np.inf, Q[lm])]
